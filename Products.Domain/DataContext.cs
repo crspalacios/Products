26eb735762b52c601bfbf8e05107838a7887e906
crspalacios <cpalacios@crealodigital.com>
2017-10-06 09:30:24 -0700

    Basic Project = Backend WEB

---
+++ Products.Domain/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Products.Domain
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
